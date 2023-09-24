105cfd0b7222d39540b80dd1ac397a8a7cd2158a
Cristian Bolaños <cbolanosmar@gmail.com>
2023-09-23 22:09:46 -0500

    fucion del menu mobil y se agrega media query

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
+const menuEmail = document.querySelector('.navbar-email'); //Tomamos la etiqueta por su clase
+const desktopMenu = document.querySelector('.desktop-menu'); //Tomamos la etiqueta por su clase
+const menuHamIcon = document.querySelector('.menu'); //Tomamos la etiqueta por su clase para mobil
+const mobileMenu = document.querySelector('.mobile-menu'); //Tomamos la etiqueta por su clase para mobil
 
-menuEmail.addEventListener('click', toggleDesktopkMenu);
+menuEmail.addEventListener('click', toggleDesktopkMenu); // llamamos la fncion al hacer clic
+menuHamIcon.addEventListener('click', toggleMobileMenu); // llamamos la fncion al hacer clic
 
 function toggleDesktopkMenu(){
-    desktopMenu.classList.toggle('inactive');
+    desktopMenu.classList.toggle('inactive'); //toggle agrega o quita la clase css segun si la tiene o no
+}
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle('inactive'); //toggle agrega o quita la clase css segun si la tiene o no
 }
